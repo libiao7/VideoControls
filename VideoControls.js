72d606937abfb946a004fc2d44fa742e41fc55d3
libiao7 <31200418+libiao7@users.noreply.github.com>
2024-09-30 14:57:11 +0800

    format 解决全屏失效

--- VideoControls.js
+++ VideoControls.js
@@ -1,4 +1,4 @@
-let pointerDownVX,pointerDownVY,ptDnX,ptDnY,pointerdownWidth = 0
+let pointerDownVX, pointerDownVY, ptDnX, ptDnY, pointerdownWidth = 0
 
 
 addEventListener('pointerdown', function (e) {
@@ -13,7 +13,7 @@ addEventListener('pointerdown', function (e) {
         pointerDownVY = e.screenY
     }
     else if (window !== top && e.target.src?.indexOf('http') === 0)
-            location.href = e.target.src.replace('http://', 'https://')
+        location.href = e.target.src.replace('http://', 'https://')
 }, { capture: true/*, passive: false*/ })
 addEventListener('touchstart', function (e) {
     if (document.fullscreenElement?.tagName == 'VIDEO') {
@@ -28,23 +28,23 @@ addEventListener('touchend', function (e) {
     }
 }, { capture: true/*, passive: false*/ })
 addEventListener('pointerup', function (e) {
-   if (document.fullscreenElement?.tagName == 'VIDEO') {
+    if (document.fullscreenElement?.tagName == 'VIDEO') {
         e.stopImmediatePropagation()
         e.preventDefault()
-        if (document.fullscreenElement==e.target && e.isPrimary) {
+        if (document.fullscreenElement == e.target && e.isPrimary) {
             let xMv = Math.abs(e.screenX - pointerDownVX)
             let yMv = Math.abs(e.screenY - pointerDownVY)
             if (xMv > 20 && xMv > yMv) {
-                e.target.currentTime +=  xMv*xMv / 625 * Math.sign(e.screenX - pointerDownVX)
+                e.target.currentTime += xMv * xMv / 625 * Math.sign(e.screenX - pointerDownVX)
                 e.target.play()
             }
             else if (yMv > 20 && yMv > xMv) {
-                if(e.screenY>pointerDownVY){
+                if (e.screenY > pointerDownVY) {
                     e.target.controls = true
                     e.target.pause()
                     e.target.playbackRate = 1
                 }
-                else if(e.screenY<pointerDownVY){
+                else if (e.screenY < pointerDownVY) {
                     e.target.controls = false
                     e.target.playbackRate = 2
                     e.target.play()
@@ -56,12 +56,17 @@ addEventListener('pointerup', function (e) {
             }
         }
     }
-    else if (!document.fullscreenElement && e.isPrimary&&pointerdownWidth>26/*&&e.screenX === ptDnX&&e.screenY === ptDnY*/) {
-        for(let video of document.querySelectorAll('video')){
+    else if (!document.fullscreenElement && e.isPrimary && pointerdownWidth > 26/*&&e.screenX === ptDnX&&e.screenY === ptDnY*/) {
+        for (let video of document.querySelectorAll('video')) {
             let videoRect = video.getBoundingClientRect();
             if (e.clientX >= videoRect.left && e.clientX <= videoRect.right && e.clientY >= videoRect.top && e.clientY <= videoRect.bottom) {
                 e.stopImmediatePropagation()
                 e.preventDefault()
+                let vel = video
+                while (vel) {
+                    vel.style.setProperty('pointer-events', 'auto', 'important')
+                    vel = vel.parentElement
+                }
                 video.requestFullscreen()
                 video.muted = false
                 video.controls = true
@@ -88,7 +93,7 @@ addEventListener('contextmenu', function (e) {
     }
 }, { capture: true })
 addEventListener("visibilitychange", () => {
-    if(document.visibilityState=='hidden'||document.hidden)
-        for(let video of document.querySelectorAll('video'))
+    if (document.visibilityState == 'hidden' || document.hidden)
+        for (let video of document.querySelectorAll('video'))
             video.pause()
 })
